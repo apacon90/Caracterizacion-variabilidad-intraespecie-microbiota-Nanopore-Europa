cec3c0bde139f3653709ed9bdb70229eafe0511b
Ana María Parodi Contreras <a.parodic@gmail.com>
2026-05-04 11:46:47 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,43 +5,42 @@
 
 # --- Base directories ---
 BASE_DIR="/path/to/rrn_operon_EU_datasets"
-PROJECT_DIR="/path/to/GROND_nr_Rep"
 
 # --- Phase 0 ---
-OUT_TRIMMED="${PROJECT_DIR}/phase0_data/trimmed_reads"
+OUT_TRIMMED="/path/to/GROND_nr_Rep/phase0_data/trimmed_reads"
 
 # --- Phase 1 ---
-MAPPINGS_DIR="${PROJECT_DIR}/phase1_data/mappings"
-SPECIES_READS_DIR="${PROJECT_DIR}/phase1_data/species_reads"
-RRN_CONSENSUS_DIR="${PROJECT_DIR}/phase1_data/Flye"
-BLAST_DB="${PROJECT_DIR}/phase1_data/blast_db/refseq207nr_nrRep_db"
-REF_DIR="${PROJECT_DIR}/phase1_data/results_phase1/ref_per_species"
-POOLS_DIR="${PROJECT_DIR}/phase1_data/results_phase1/pool_mappings"
-VCF_RAW_DIR="${PROJECT_DIR}/phase1_data/results_phase1/vcf_raw"
-VCF_FILTERED_DIR="${PROJECT_DIR}/phase1_data/results_phase1/vcf_filtered"
+MAPPINGS_DIR="/path/to/GROND_nr_Rep/phase1_data/mappings"
+SPECIES_READS_DIR="/path/to/GROND_nr_Rep/phase1_data/species_reads"
+RRN_CONSENSUS_DIR="/path/to/GROND_nr_Rep/phase1_data/Flye"
+BLAST_DB="/path/to/GROND_nr_Rep/phase1_data/blast_db/refseq207nr_nrRep_db"
+REF_DIR="/path/to/GROND_nr_Rep/phase1_data/results_phase1/ref_per_species"
+POOLS_DIR="/path/to/GROND_nr_Rep/phase1_data/results_phase1/pool_mappings"
+VCF_RAW_DIR="/path/to/GROND_nr_Rep/phase1_data/results_phase1/vcf_raw"
+VCF_FILTERED_DIR="/path/to/GROND_nr_Rep/phase1_data/results_phase1/vcf_filtered"
 
 # --- Phase 2 ---
-FASTA_ALL="${PROJECT_DIR}/phase2_data/fasta_all"
-FASTA_N="${PROJECT_DIR}/phase2_data/fasta_n100_or_max"
-AUX_DIR="${PROJECT_DIR}/phase2_data/aux"
-IND_BAM_DIR="${PROJECT_DIR}/phase2_data/results_phase2/individual_mappings"
-IND_VCF_DIR="${PROJECT_DIR}/phase2_data/results_phase2/vcf_raw"
-IND_VCF_FILT_DIR="${PROJECT_DIR}/phase2_data/results_phase2/vcf_filtered"
-TSV_DIR="${PROJECT_DIR}/phase2_data/variants_tsv"
+FASTA_ALL="/path/to/GROND_nr_Rep/phase2_data/fasta_all"
+FASTA_N="/path/to/GROND_nr_Rep/phase2_data/fasta_n100_or_max"
+AUX_DIR="/path/to/GROND_nr_Rep/phase2_data/aux"
+IND_BAM_DIR="/path/to/GROND_nr_Rep/phase2_data/results_phase2/individual_mappings"
+IND_VCF_DIR="/path/to/GROND_nr_Rep/phase2_data/results_phase2/vcf_raw"
+IND_VCF_FILT_DIR="/path/to/GROND_nr_Rep/phase2_data/results_phase2/vcf_filtered"
+TSV_DIR="/path/to/GROND_nr_Rep/phase2_data/variants_tsv"
 
 # --- Reference files ---
-DB_REF="${PROJECT_DIR}/reference/refseq207nr_nrRep.fna"
-DB_REF_FASTA="${PROJECT_DIR}/reference/refseq207nr_nrRep.fna"
-DB_MMI="${PROJECT_DIR}/reference/refseq207nr_nrRep.mmi"
-TAX_FILE="${PROJECT_DIR}/reference/refseq207nr_taxRep.tsv"
-CODE2NAME_FILE="${PROJECT_DIR}/reference/code2name.tsv"
+DB_REF="/path/to/GROND_nr_Rep/reference/refseq207nr_nrRep.fna"
+DB_REF_FASTA="/path/to/GROND_nr_Rep/reference/refseq207nr_nrRep.fna"
+DB_MMI="/path/to/GROND_nr_Rep/reference/refseq207nr_nrRep.mmi"
+TAX_FILE="/path/to/GROND_nr_Rep/reference/refseq207nr_taxRep.tsv"
+CODE2NAME_FILE="/path/to/GROND_nr_Rep/reference/code2name.tsv"
 
 # --- Metadata and results ---
-META_FILE="${PROJECT_DIR}/metadata/sample_metadata.tsv"
-TOP50_TSV="${PROJECT_DIR}/metadata/Top50_species_abundance_median.tsv"
-SELECT="${PROJECT_DIR}/metadata/select_sp.tsv"
-RESULTS_DIR="${PROJECT_DIR}/metadata"
-PAIRS_FILE="${PROJECT_DIR}/phase2_data/pairs_14bacteria_samples_ge100.tsv"
+META_FILE="/path/to/GROND_nr_Rep/metadata/sample_metadata.tsv"
+TOP50_TSV="/path/to/GROND_nr_Rep/metadata/Top50_species_abundance_median.tsv"
+SELECT="/path/to/GROND_nr_Rep/metadata/select_sp.tsv"
+RESULTS_DIR="/path/to/GROND_nr_Rep/metadata"
+PAIRS_FILE="/path/to/GROND_nr_Rep/phase2_data/pairs_14bacteria_samples_ge100.tsv"
 
 # --- Tools ---
 BARRNAP="/path/to/barrnap/bin/barrnap"
